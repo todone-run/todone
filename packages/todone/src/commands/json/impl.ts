c84a3d03a88c48b2a2ff7189da7f31b2107c2027
Carlos Precioso <github@precioso.design>
2026-08-28 11:26:10 +0200

    Add ignore

--- packages/todone/src/commands/json/impl.ts
+++ packages/todone/src/commands/json/impl.ts
@@ -3,7 +3,10 @@ import { jsonReporter } from "#/lib/reporters/json";
 import { loadConfigFile } from "../../lib/config";
 import { JsonCommand } from "./index";
 
-export default async (_: JsonCommand) => {
+export default async (
+  // oxlint-disable-next-line no-unused-vars
+  _: JsonCommand,
+) => {
   const config = await loadConfigFile();
   await run(config, { reporter: jsonReporter() });
 };
