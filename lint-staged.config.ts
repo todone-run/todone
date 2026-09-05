72498885cda2a1b09f9dfc8340e3fcfc2598851f
Carlos Precioso <github@precioso.design>
2026-09-05 14:53:09 +0200

    chore: use `defineConfig` helper in lint-staged

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,8 +1,10 @@
+import { defineConfig } from "lint-staged/config";
+
 const prettier = "prettier --write --ignore-unknown";
 const oxlint = "oxlint --fix";
 
-export default {
+export default defineConfig({
   "*": prettier,
   "*.{js,jsx,ts,tsx,mjs,cjs}": [oxlint, prettier],
   "yarn.lock": () => "yarn dedupe",
-};
+});
